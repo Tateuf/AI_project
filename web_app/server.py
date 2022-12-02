8821e11ba7d3555188a255f7a33b017fb482bbe3
Tateuf <logan.noel.h@gmail.com>
2022-12-02 15:30:25 +0100

    digitRecoPOC

--- web_app/server.py
+++ web_app/server.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template, request
 import os
 from werkzeug.utils import secure_filename
+import digitRecognition
 
 UPLOAD_FOLDER = os.path.join('static', 'uploads')
 
@@ -14,6 +15,7 @@ def index():
 @app.route('/digit', methods=('GET', 'POST'))
 def digit():
    img_filename=""
+   guess = "waiting input"
    if request.method == 'POST':
         # Upload file flask
         uploaded_img = request.files['uploaded-file']
@@ -21,9 +23,11 @@ def digit():
         img_filename = secure_filename(uploaded_img.filename)
         # Upload file to database (defined uploaded folder in static path)
         uploaded_img.save("web_app/static/"+img_filename)
+        guess = digitRecognition.digit_recognition("web_app/static/"+img_filename)
       #   # Storing uploaded file path in flask session
       #   session['uploaded_img_file_path'] = os.path.join(app.config['UPLOAD_FOLDER'], img_filename)
-   return render_template('digit_check.html', preview = "static/"+img_filename)
+   
+   return render_template('digit_check.html', preview = "static/"+img_filename, guess = guess)
 
 @app.route('/home')
 def home():
