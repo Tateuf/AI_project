8c929cfbf80cfbc1f5304c04fe4a5081b8f5dd82
Tateuf <logan.noel.h@gmail.com>
2023-01-07 17:19:29 +0100

    emnist_model_propre

--- web_app/emnist_training.py
+++ web_app/emnist_training.py
@@ -6,114 +6,58 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
 from keras.models import Sequential, load_model
-from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras import layers
 from keras.layers import *
 from keras.utils import np_utils
 from tqdm import tqdm
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau,ModelCheckpoint
 
-#for dirname, _, filenames in os.walk('kaggle/input'):
-#    for filename in filenames:
-#        print(os.path.join(dirname, filename))
-        
-train_df = pd.read_csv('kaggle/input/emnist/emnist-balanced-train.csv', header=None)
-test_df = pd.read_csv('kaggle/input/emnist/emnist-balanced-test.csv', header=None)
-#print(train_df.head())
+testing_letter = pd.read_csv('AI_project/web_app/kaggle/input/emnist/emnist-letters-test.csv')
+training_letter = pd.read_csv('AI_project/web_app/kaggle/input/emnist/emnist-letters-train.csv')
 
-label_map = pd.read_csv("kaggle/input/emnist/emnist-balanced-mapping.txt", 
-                        delimiter = ' ', 
-                        index_col=0, 
-                        header=None, 
-                        squeeze=True)
-#print(label_map.head())
+y1 = np.array(training_letter.iloc[:,0].values)
+x1 = np.array(training_letter.iloc[:,1:].values)
 
-#Initialising an empty dictionary
-label_dictionary = {}
+y2 = np.array(testing_letter.iloc[:,0].values)
+x2 = np.array(testing_letter.iloc[:,1:].values)
 
-i = 0
-#Running a loop for ASCII equivalent to character conversion
-for index, label in enumerate(label_map):
-    #if i>=10:
-        label_dictionary[index-10] = chr(label)
-    #i += 1
-#print(label_dictionary)
+train_images = x1 / 255.0
+test_images = x2 / 255.0
 
-#train_df_new = train_df[train_df[0].isin(np.arange(10, 47))] #onlyletter
-#train_df_new.loc[train_df_new[0] >= 10 , 0] = train_df_new[0] - 10
+train_images_number = train_images.shape[0]
+train_images_height = 28
+train_images_width = 28
+train_images_size = train_images_height*train_images_width
 
-#test_df_new = test_df[test_df[0].isin(np.arange(10, 47))] #onlyletter
-#test_df_new.loc[test_df_new[0] >= 10 , 0] = test_df_new[0] - 10
+train_images = train_images.reshape(train_images_number, train_images_height, train_images_width, 1)
 
-#print(train_df_new)
-train_df.reset_index(inplace=True)
-test_df.reset_index(inplace=True)
-#print(train_df_new)
+test_images_number = test_images.shape[0]
+test_images_height = 28
+test_images_width = 28
+test_images_size = test_images_height*test_images_width
 
-x_train = train_df.loc[:, 1:]
-y_train = train_df.loc[:, 0]
+test_images = test_images.reshape(test_images_number, test_images_height, test_images_width, 1)
 
-x_test = test_df.loc[:, 1:]
-y_test = test_df.loc[:, 0]
-#print(x_train.shape, y_train.shape)
+number_of_classes = 37
 
-def flip_and_rotate(image):
-    W = 28
-    H = 28
-    image = image.reshape(W, H)
-    image = np.fliplr(image)
-    image = np.rot90(image)
-    return image
+y1 = tf.keras.utils.to_categorical(y1, number_of_classes)
+y2 = tf.keras.utils.to_categorical(y2, number_of_classes)
 
-x_train = np.apply_along_axis(flip_and_rotate, 1, x_train.values)
-x_test = np.apply_along_axis(flip_and_rotate, 1, x_test.values)
-#print(x_train.shape)
+train_x,test_x,train_y,test_y = train_test_split(train_images,y1,test_size=0.10,random_state = 2)
 
-x_train = x_train.astype('float32') / 255
-x_test = x_test.astype('float32') /255
-#print(x_train[0])
+model = tf.keras.Sequential([ 
+    tf.keras.layers.Conv2D(32,3,input_shape=(28,28,1)),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Flatten(input_shape=(28,28,1)),
+    tf.keras.layers.Dense(512,activation='relu'),
+    tf.keras.layers.Dense(128,activation='relu'),
+    tf.keras.layers.Dense(number_of_classes,activation='softmax')
+])
 
-number_of_classes = y_train.nunique()
-#print(number_of_classes)
+model.compile(optimizer='rmsprop',loss='categorical_crossentropy',metrics=['accuracy'])
 
-y_train = np_utils.to_categorical(y_train, number_of_classes)
-y_test = np_utils.to_categorical(y_test, number_of_classes)
-#print(y_train.shape)
+MCP = ModelCheckpoint('AI_project/web_app/emnist_model.h5',verbose=1,save_best_only=True,monitor='val_accuracy',mode='max')
+ES = EarlyStopping(monitor='val_accuracy',min_delta=0,verbose=0,restore_best_weights = True,patience=3,mode='max')
+RLP = ReduceLROnPlateau(monitor='val_loss',patience=3,factor=0.2,min_lr=0.0001)
 
-x_train = x_train.reshape(-1, 28, 28, 1)
-x_test = x_test.reshape(-1, 28, 28, 1)
-
-x_train, x_val, y_train, y_val = train_test_split(x_train, 
-                                                  y_train, 
-                                                  test_size= 0.1, 
-                                                  random_state=88)
-
-model = Sequential()
-
-model.add(layers.Conv2D(filters=32, kernel_size=(5,5), padding='same', activation='relu', input_shape=(28, 28, 1)))
-model.add(layers.MaxPool2D(strides=2))
-model.add(layers.Conv2D(filters=48, kernel_size=(5,5), padding='valid', activation='relu'))
-model.add(layers.MaxPool2D(strides=2))
-model.add(layers.Flatten())
-model.add(layers.Dense(256, activation='relu'))
-model.add(layers.Dense(84, activation='relu'))
-model.add(layers.Dense(number_of_classes, activation='softmax'))
-
-model.summary()
-
-optimizer_name = 'adam'
-
-model.compile(loss='categorical_crossentropy', optimizer=optimizer_name, metrics=['accuracy'])
-
-early_stopping = EarlyStopping(monitor='val_loss', patience=5, verbose=1, mode='min')
-mcp_save = ModelCheckpoint('emnist_model.h5', save_best_only=True, monitor='val_loss', verbose=1, mode='auto')
-
-history = model.fit(x_train,
-                    y_train, 
-                    epochs=30, 
-                    batch_size=32, 
-                    verbose=1, 
-                    validation_split=0.1,
-                    callbacks=[early_stopping, mcp_save])
-
-model = load_model('emnist_model.h5')
-model.summary()
+history = model.fit(train_x,train_y,epochs=5,validation_data=(test_x,test_y),callbacks=[MCP,ES,RLP])
